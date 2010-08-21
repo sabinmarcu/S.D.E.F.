ac8b38f2d86a360ac4c74110b7a080e9aa92bd5a
Marcu Sabin <sabinmarcu@algorimetrica.info>
2010-08-21 15:18:54 +0300

    Created the initial fully functional desktop environment ... awating new orders boss ...

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,133 @@
+function Window(id)	{
+	this.id = id;
+
+	//	Construct / Destruct Handler
+	this.toggle = function()	{	
+		if (!$(".container#"+id).length)	{
+			this.y = '100px';
+			this.x = ($(window).width() - 600) / 2 +"px";
+			this.width = '600px';	
+			this.height = '70%';
+			this.init();
+		}
+		else this.del(id);
+	}
+	
+	
+	
+	
+	// Constructor Method
+	this.init = function()	{
+		if (!$(".container#"+this.id).length)
+			$("body").append(
+				"<div class='container drag' id='"+ id +
+				"' style='left:"+ this.x +
+				";top:"+ this.y +
+				";height:"+ this.height +
+				";width:"+ this.width +
+				"'></div>"
+			).find(".container#"+id)
+			.draggable({
+				cancel: 'section', 
+				drag: function(){
+					windows[this.id].x = $(this).css("left");
+					windows[this.id].y = $(this).css("top");
+				}
+			})
+			.resizable({
+				resize: function(){
+					windows[this.id].width = $(this).css("width");
+					windows[this.id].height = $(this).css("height");
+				}
+			})
+			.append("<div class='windowb' id='close'>X</div><div class='windowb' id='maximize'>[]</div><div class='windowb' id='minimize'>-</div><section></section>")
+			.animate({opacity: 0.95}, 500)
+			.find("section").load(id+".html")
+			.parent().find(".windowb")
+			.click( function(){
+				windows[id].mod(
+					$(this).attr('id'), 
+					$(this).parent().attr('id')
+				);});
+	}
+	
+	
+	
+	//Deconstructor Method
+	this.del = function()	{
+		$(".container#"+this.id).animate({opacity: 0}, 500, 'linear', function()	{$(this).remove()});
+	}
+	
+	
+	
+	//Window Action Controller
+	this.mod = function(action){
+	switch (action)	{
+		case 'close'	:	this.del();	break;
+		case 'maximize'	:	
+			if (!$(".container#"+this.id).hasClass("maximized"))	this.maximize();
+			else this.restore();	
+		break;
+		case 'minimize'	:
+			if (!$(".container#"+this.id).hasClass("minimized") && !$(".container#"+this.id).hasClass("maximized"))	this.minimize();
+			else if (!$(".container#"+this.id).hasClass("maximized")) this.restore();
+		}
+	}
+	
+	//Maximize Method
+	this.maximize = function()	{
+		$(".container#"+this.id).animate({height: $(window).height(), width: $(window).width(), top: 0, left: 0}, 500).draggable('disable').addClass("maximized");	
+	}
+	
+	//Restore Method
+	this.restore = function()	{
+		$(".container#"+this.id).animate({height: this.height, width: this.width, top: this.y, left: this.x, opacity: 1}, 500).draggable('enable').removeClass("maximized").removeClass("minimized");
+		$("footer li#"+this.id).animate({opacity: 0}, 500, 'linear', function()	{$(this).remove()});
+	}
+	
+	//Minimize Method
+	this.minimize = function()	{
+		$("footer ul").append("<li id='"+this.id+"'>"+this.id+"</li>").find("li#"+this.id).animate({opacity: 1}, 500);
+		$("footer li").click(function()	{
+			windows[this.id].restore();
+		});
+		$(".container#"+this.id).animate({opacity: 0}, 500).addClass("minimized");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+windows = new Array();
+
 $(document).ready(function()	{
 	$("header div").hide().parent().find("img").hover(function(){$(this).parent().find("div").css({display: 'block', opacity: 0}).animate({opacity: 1}, 500, 'linear');},function(){$(this).parent().find("div").animate({opacity: 0}, 500, 'linear', function(){$(this).hide()});});
 	$("nav li").click(function() {
-		var id = $(this).attr('id');
-		if (!$(".container#"+id).length)
-		$("body").append("<div class='container' id='"+id+"'></div>").find(".container#"+id).draggable().resizable().animate({opacity: 1}, 500).load(id+".html");
-		else $(".container#"+id).animate({opacity: 0}, 500, 'linear', function()	{$(this).remove()});
+	var id = $(this).attr('id');
+		if (!windows[id])	
+			windows[id] = new Window(id);
+			windows[id].toggle();
 	});
-	
 });
+
 
